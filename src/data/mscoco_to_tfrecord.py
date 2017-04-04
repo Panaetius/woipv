Fixes bug with IoU calculation and several minor adjustments

src/data/mscoco_to_tfrecord.py
@@ -6,6 +6,7 @@ import os
 import cv2
 
 image_size = (600, 600)
+min_bbox_size = 20
 
 dataDir = "%s/../../data/raw/MSCOCO" % os.path.dirname(os.path.realpath(
     __file__))
@@ -50,14 +51,13 @@ for img in images:
     img_data = cv2.resize(img_data, (image_size[0], image_size[1]))
     img_data = img_data[...,::-1].copy()
 
-    anns = [ann for ann in anns if ann['iscrowd'] == 0]
+    anns = [ann for ann in anns if ann['iscrowd'] == 0 and ann["bbox"][2] * x_scale > min_bbox_size and ann["bbox"][3] * y_scale > min_bbox_size]
 
     annCatIds = [ann["category_id"] - 1 for ann in anns]
 
     annBBoxes = [np.multiply(np.asarray(ann["bbox"]), [x_scale, y_scale,
                                                       x_scale, y_scale])
-                          for ann in
-                 anns]
+                          for ann in anns]
 
     annBBoxes = [np.asarray([ann[0] + ann[2]/2.0, ann[1] + ann[3]/2.0,
                              ann[2], ann[3]]).tolist()

src/models/mscoco_input.py
@@ -103,23 +103,26 @@ class MSCOCOInputProducer(object):
 
         result = self.__read(filename_queue)
 
-        distorted_image = tf.cast(result.image_raw, tf.float32)
+        distorted_image = result.image_raw
+
+        distorted_image = tf.cast(distorted_image, tf.float32)
+
         distorted_image = tf.reshape(distorted_image,
                                      [self.width, self.height, 3])
 
         # Image processing for training the network. Note the many random
         # distortions applied to the image.
-        distorted_image = tf.image.random_brightness(distorted_image,
-                                                     max_delta=35)
+        #distorted_image = tf.image.random_brightness(distorted_image,
+        #                                             max_delta=35)
         distorted_image = tf.image.random_contrast(distorted_image,
                                                    lower=0.4, upper=1.4)
-        distorted_image = tf.image.random_hue(distorted_image, max_delta=0.01)
+        distorted_image = tf.image.random_hue(distorted_image, max_delta=0.03)
 
         # Subtract off the mean and divide by the variance of the pixels.
-        float_image = tf.image.per_image_standardization(distorted_image)
+        distorted_image = tf.image.per_image_standardization(distorted_image)
 
         # Ensure that the random shuffling has good mixing properties.
-        min_fraction_of_examples_in_queue = 0.0025
+        min_fraction_of_examples_in_queue = 0.01
         min_queue_examples = int(self.num_examples_per_epoch *
                                  min_fraction_of_examples_in_queue)
 
@@ -127,7 +130,7 @@ class MSCOCOInputProducer(object):
               'This will take a few minutes.' % min_queue_examples)
 
         images, category_batch, bbox_batch = tf.train.shuffle_batch(
-            [float_image, result.categories, result.bboxes],
+            [distorted_image, result.categories, result.bboxes],
             batch_size=self.batch_size,
             num_threads=self.num_preprocess_threads,
             capacity=min_queue_examples + 3 * self.batch_size,

src/models/train_model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.python.client import timeline
+from tensorflow.python import debug as tf_debug
 import os
 import time
 from datetime import datetime
@@ -10,7 +11,7 @@ from mscoco_input import MSCOCOInputProducer
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('train_dir', '/tmp/woipv_train',
+tf.app.flags.DEFINE_string('train_dir', '/training/woipv_train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
 tf.app.flags.DEFINE_integer('max_steps', 100000,
@@ -24,8 +25,8 @@ class Config(object):
     chkpt_path = "%s/../../models/transfer_chkpt/" % os.path.dirname(
             os.path.realpath(__file__))
     batch_size = 4
-    num_examples_per_epoch = 8000
-    num_epochs_per_decay = 1
+    num_examples_per_epoch = 16000
+    num_epochs_per_decay = 8
     is_training = True
     num_classes = 90
     initial_learning_rate = 1e-5
@@ -33,14 +34,14 @@ class Config(object):
     width = 600
     height = 600
     min_box_size = 1
-    rcnn_cls_loss_weight = 0.1
-    rcnn_reg_loss_weight = 0.1
-    rpn_cls_loss_weight = 1.0
-    rpn_reg_loss_weight = 1.0
+    rcnn_cls_loss_weight = 0.02 / 90.0
+    rcnn_reg_loss_weight = 0.005
+    rpn_cls_loss_weight = 1.35 / 171.0
+    rpn_reg_loss_weight = 0.7
     background_weight = 0.2
     dropout_prob = 0.5 # not used yet
     weight_decay = 0.0001
-    restore_from_chkpt = True
+    restore_from_chkpt = False
     variables_to_restore = ['first_layer/weights:0', 'first_layer/Variable:0', 'first_layer/Variable_1:0', 'first_layer/Variable_2:0', 'first_layer/Variable_3:0', 'reslayer_64_0/sub1/weights:0', 'reslayer_64_0/sub1/Variable:0', 'reslayer_64_0/sub1/Variable_1:0', 'reslayer_64_0/sub1/Variable_2:0', 'reslayer_64_0/sub1/Variable_3:0', 'reslayer_64_0/sub2/weights:0', 'reslayer_64_0/sub2/Variable:0', 'reslayer_64_0/sub2/Variable_1:0', 'reslayer_64_0/sub2/Variable_2:0', 'reslayer_64_0/sub2/Variable_3:0', 'reslayer_64_1/sub1/weights:0', 'reslayer_64_1/sub1/Variable:0', 'reslayer_64_1/sub1/Variable_1:0', 'reslayer_64_1/sub1/Variable_2:0', 'reslayer_64_1/sub1/Variable_3:0', 'reslayer_64_1/sub2/weights:0', 'reslayer_64_1/sub2/Variable:0', 'reslayer_64_1/sub2/Variable_1:0', 'reslayer_64_1/sub2/Variable_2:0', 'reslayer_64_1/sub2/Variable_3:0', 'reslayer_64_2/sub1/weights:0', 'reslayer_64_2/sub1/Variable:0', 'reslayer_64_2/sub1/Variable_1:0', 'reslayer_64_2/sub1/Variable_2:0', 'reslayer_64_2/sub1/Variable_3:0', 'reslayer_64_2/sub2/weights:0', 'reslayer_64_2/sub2/Variable:0', 'reslayer_64_2/sub2/Variable_1:0', 'reslayer_64_2/sub2/Variable_2:0', 'reslayer_64_2/sub2/Variable_3:0', 'reslayer_downsample_128/sub1/weights:0', 'reslayer_downsample_128/sub1/Variable:0', 'reslayer_downsample_128/sub1/Variable_1:0', 'reslayer_downsample_128/sub1/Variable_2:0', 'reslayer_downsample_128/sub1/Variable_3:0', 'reslayer_downsample_128/sub2/weights:0', 'reslayer_downsample_128/sub2/Variable:0', 'reslayer_downsample_128/sub2/Variable_1:0', 'reslayer_downsample_128/sub2/Variable_2:0', 'reslayer_downsample_128/sub2/Variable_3:0', 'reslayer_128_0/sub1/weights:0', 'reslayer_128_0/sub1/Variable:0', 'reslayer_128_0/sub1/Variable_1:0', 'reslayer_128_0/sub1/Variable_2:0', 'reslayer_128_0/sub1/Variable_3:0', 'reslayer_128_0/sub2/weights:0', 'reslayer_128_0/sub2/Variable:0', 'reslayer_128_0/sub2/Variable_1:0', 'reslayer_128_0/sub2/Variable_2:0', 'reslayer_128_0/sub2/Variable_3:0', 'reslayer_128_1/sub1/weights:0', 'reslayer_128_1/sub1/Variable:0', 'reslayer_128_1/sub1/Variable_1:0', 'reslayer_128_1/sub1/Variable_2:0', 'reslayer_128_1/sub1/Variable_3:0', 'reslayer_128_1/sub2/weights:0', 'reslayer_128_1/sub2/Variable:0', 'reslayer_128_1/sub2/Variable_1:0', 'reslayer_128_1/sub2/Variable_2:0', 'reslayer_128_1/sub2/Variable_3:0', 'reslayer_128_2/sub1/weights:0', 'reslayer_128_2/sub1/Variable:0', 'reslayer_128_2/sub1/Variable_1:0', 'reslayer_128_2/sub1/Variable_2:0', 'reslayer_128_2/sub1/Variable_3:0', 'reslayer_128_2/sub2/weights:0', 'reslayer_128_2/sub2/Variable:0', 'reslayer_128_2/sub2/Variable_1:0', 'reslayer_128_2/sub2/Variable_2:0', 'reslayer_128_2/sub2/Variable_3:0', 'reslayer_downsample_256/sub1/weights:0', 'reslayer_downsample_256/sub1/Variable:0', 'reslayer_downsample_256/sub1/Variable_1:0', 'reslayer_downsample_256/sub1/Variable_2:0', 'reslayer_downsample_256/sub1/Variable_3:0', 'reslayer_downsample_256/sub2/weights:0', 'reslayer_downsample_256/sub2/Variable:0', 'reslayer_downsample_256/sub2/Variable_1:0', 'reslayer_downsample_256/sub2/Variable_2:0', 'reslayer_downsample_256/sub2/Variable_3:0', 'reslayer_256_0/sub1/weights:0', 'reslayer_256_0/sub1/Variable:0', 'reslayer_256_0/sub1/Variable_1:0', 'reslayer_256_0/sub1/Variable_2:0', 'reslayer_256_0/sub1/Variable_3:0', 'reslayer_256_0/sub2/weights:0', 'reslayer_256_0/sub2/Variable:0', 'reslayer_256_0/sub2/Variable_1:0', 'reslayer_256_0/sub2/Variable_2:0', 'reslayer_256_0/sub2/Variable_3:0', 'reslayer_256_1/sub1/weights:0', 'reslayer_256_1/sub1/Variable:0', 'reslayer_256_1/sub1/Variable_1:0', 'reslayer_256_1/sub1/Variable_2:0', 'reslayer_256_1/sub1/Variable_3:0', 'reslayer_256_1/sub2/weights:0', 'reslayer_256_1/sub2/Variable:0', 'reslayer_256_1/sub2/Variable_1:0', 'reslayer_256_1/sub2/Variable_2:0', 'reslayer_256_1/sub2/Variable_3:0', 'reslayer_256_2/sub1/weights:0', 'reslayer_256_2/sub1/Variable:0', 'reslayer_256_2/sub1/Variable_1:0', 'reslayer_256_2/sub1/Variable_2:0', 'reslayer_256_2/sub1/Variable_3:0', 'reslayer_256_2/sub2/weights:0', 'reslayer_256_2/sub2/Variable:0', 'reslayer_256_2/sub2/Variable_1:0', 'reslayer_256_2/sub2/Variable_2:0', 'reslayer_256_2/sub2/Variable_3:0', 'reslayer_256_3/sub1/weights:0', 'reslayer_256_3/sub1/Variable:0', 'reslayer_256_3/sub1/Variable_1:0', 'reslayer_256_3/sub1/Variable_2:0', 'reslayer_256_3/sub1/Variable_3:0', 'reslayer_256_3/sub2/weights:0', 'reslayer_256_3/sub2/Variable:0', 'reslayer_256_3/sub2/Variable_1:0', 'reslayer_256_3/sub2/Variable_2:0', 'reslayer_256_3/sub2/Variable_3:0', 'reslayer_256_4/sub1/weights:0', 'reslayer_256_4/sub1/Variable:0', 'reslayer_256_4/sub1/Variable_1:0', 'reslayer_256_4/sub1/Variable_2:0', 'reslayer_256_4/sub1/Variable_3:0', 'reslayer_256_4/sub2/weights:0', 'reslayer_256_4/sub2/Variable:0', 'reslayer_256_4/sub2/Variable_1:0', 'reslayer_256_4/sub2/Variable_2:0', 'reslayer_256_4/sub2/Variable_3:0', 'reslayer_downsample_512/sub1/weights:0', 'reslayer_downsample_512/sub1/Variable:0', 'reslayer_downsample_512/sub1/Variable_1:0', 'reslayer_downsample_512/sub1/Variable_2:0', 'reslayer_downsample_512/sub1/Variable_3:0', 'reslayer_downsample_512/sub2/weights:0', 'reslayer_downsample_512/sub2/Variable:0', 'reslayer_downsample_512/sub2/Variable_1:0', 'reslayer_downsample_512/sub2/Variable_2:0', 'reslayer_downsample_512/sub2/Variable_3:0', 'reslayer_512_0/sub1/weights:0', 'reslayer_512_0/sub1/Variable:0', 'reslayer_512_0/sub1/Variable_1:0', 'reslayer_512_0/sub1/Variable_2:0', 'reslayer_512_0/sub1/Variable_3:0', 'reslayer_512_0/sub2/weights:0', 'reslayer_512_0/sub2/Variable:0', 'reslayer_512_0/sub2/Variable_1:0', 'reslayer_512_0/sub2/Variable_2:0', 'reslayer_512_0/sub2/Variable_3:0', 'reslayer_512_1/sub1/weights:0', 'reslayer_512_1/sub1/Variable:0', 'reslayer_512_1/sub1/Variable_1:0', 'reslayer_512_1/sub1/Variable_2:0', 'reslayer_512_1/sub1/Variable_3:0', 'reslayer_512_1/sub2/weights:0', 'reslayer_512_1/sub2/Variable:0', 'reslayer_512_1/sub2/Variable_1:0', 'reslayer_512_1/sub2/Variable_2:0', 'reslayer_512_1/sub2/Variable_3:0']
 
 def train():
@@ -87,6 +88,7 @@ def train():
         # Start running operations on the Graph.
         print("Running init %.3f" % time.time())
         sess = tf.Session(config=config)
+        # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
         sess.run(init)
 
         if cfg.restore_from_chkpt:
@@ -108,16 +110,40 @@ def train():
 
         summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
 
-        # run_metadata = tf.RunMetadata()
+        #run_metadata = tf.RunMetadata()
         print("Started training %.3f" % time.time())
         for step in range(FLAGS.max_steps):
             start_time = time.time()
             _, loss_value = sess.run([train_op, loss])
-                                     # options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),
-                                     # run_metadata=run_metadata)
+                                     #options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),
+                                     #run_metadata=run_metadata)
             duration = time.time() - start_time
 
             assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+            #tf.contrib.tfprof.model_analyzer.print_model_analysis(
+            #    tf.get_default_graph(),
+            #    run_meta=run_metadata,
+            #    tfprof_options={
+            #        'max_depth': 10000,
+            #        'min_bytes': 1,  # Only >=1
+            #        'min_micros': 1,  # Only >=1
+            #        'min_params': 0,
+            #        'min_float_ops': 0,
+            #        'device_regexes': ['.*'],
+            #        'order_by': 'name',
+            #        'account_type_regexes': ['.*'],
+            #        'start_name_regexes': ['.*'],
+            #        'trim_name_regexes': [],
+            #        'show_name_regexes': ['.*'],
+            #        'hide_name_regexes': [],
+            #        'account_displayed_op_only': True,
+            #        'select': ['micros'],
+            #        'viz': False,
+            #        'dump_to_file': ''
+            #    })
+
+            #return
 
             if step % 25 == 0:
                 num_examples_per_step = cfg.batch_size